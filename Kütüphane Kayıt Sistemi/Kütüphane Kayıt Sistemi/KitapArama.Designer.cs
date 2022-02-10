dcce49c6c2dfe2c48212c9c30455a82830739d2b
SalihYalcin47 <99169193+SalihYalcin47@users.noreply.github.com>
2022-02-10 12:31:05 +0300

    Add files via upload

---
+++ Kütüphane Kayıt Sistemi/Kütüphane Kayıt Sistemi/KitapArama.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Kütüphane_Kayıt_Sistemi
+{
+    partial class KitapArama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aramak İstediğiniz Kitabın Adı:  ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(398, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 37);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 157);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(762, 593);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(191, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(276, 32);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Kitap Arama Ekranı";
+            // 
+            // KitapArama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 762);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "KitapArama";
+            this.Text = "KitapArama";
+            this.Load += new System.EventHandler(this.KitapArama_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Kütüphane Kayıt Sistemi/Kütüphane Kayıt Sistemi/KitapSilme.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Kütüphane_Kayıt_Sistemi
+{
+    public partial class KitapSilme : Form
+    {
+        public KitapSilme()
+        {
+            InitializeComponent();
+        }
+        SqlConnection Baglanti = new SqlConnection("Data Source=localhost ; Initial Catalog=Kütüphane ; Integrated Security=True");
+        DataTable tablo = new DataTable();
+        private void KitapSilme_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter sorgu = new SqlDataAdapter("Select * From Kütüphanem order by İd",Baglanti);
+            sorgu.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DataTable tablo2 = new DataTable();
+            textBox1.Text = textBox1.Text.ToUpper();
+            textBox1.SelectionStart = textBox1.Text.Length;
+            SqlDataAdapter sorgu2 = new SqlDataAdapter("Select * From Kütüphanem Where İd like'"+textBox1.Text+"'", Baglanti);
+            sorgu2.Fill(tablo2);
+            dataGridView1.DataSource=tablo2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Refresh();
+            DataTable tablo3 = new DataTable();
+            SqlDataAdapter sorgu3 = new SqlDataAdapter("Delete From Kütüphanem Where İd ='"+textBox1.Text+"'",Baglanti);
+            sorgu3.Fill(tablo3);
+            dataGridView1.DataSource= tablo3;
+        }
+    }
+}
